5bd2462ba12722f65baf2b00b205f2842c7d121b
GM <giovanna.occhialini@wsu.edu>
2026-04-28 05:48:54 +0000

    added more recipe data and category filtering

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let recipesData = [];
+
 document.addEventListener("DOMContentLoaded", function () {
   fetch("recipes.json")
     .then(function (response) {
@@ -5,7 +7,9 @@ document.addEventListener("DOMContentLoaded", function () {
     })
     .then(function (data) {
       console.log(data);
-      displayRecipes(data);
+      recipesData = data;
+      displayRecipes(recipesData);
+      setButtons();
     })
     .catch(function (error) {
       console.log("Error loading recipes:", error);
@@ -13,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function displayRecipes(recipes) {
-  const container = document.getElementById("recipeContainer");
+  let container = document.getElementById("recipeContainer");
   container.innerHTML = "";
 
   for (let i = 0; i < recipes.length; i++) {
@@ -21,7 +25,42 @@ function displayRecipes(recipes) {
       <div class="recipe-card">
         <img src="${recipes[i].image}" alt="${recipes[i].name}">
         <p class="recipe-title">${recipes[i].name}</p>
+        <p class="recipe-description">${recipes[i].shortDescription}</p>
       </div>
     `;
   }
+}
+
+function setButtons() {
+  let allBtn = document.getElementById("allBtn");
+  let sweetBtn = document.getElementById("sweetBtn");
+  let savoryBtn = document.getElementById("savoryBtn");
+
+  allBtn.addEventListener("click", function () {
+    displayRecipes(recipesData);
+  });
+
+  sweetBtn.addEventListener("click", function () {
+    let sweetRecipes = [];
+
+    for (let i = 0; i < recipesData.length; i++) {
+      if (recipesData[i].category == "sweet") {
+        sweetRecipes.push(recipesData[i]);
+      }
+    }
+
+    displayRecipes(sweetRecipes);
+  });
+
+  savoryBtn.addEventListener("click", function () {
+    let savoryRecipes = [];
+
+    for (let i = 0; i < recipesData.length; i++) {
+      if (recipesData[i].category == "savory") {
+        savoryRecipes.push(recipesData[i]);
+      }
+    }
+
+    displayRecipes(savoryRecipes);
+  });
 }
